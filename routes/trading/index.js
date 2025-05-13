e6206ef6e8f64875e569488c90ae5355570a0403
thenasty1337 <114357418+thenasty1337@users.noreply.github.com>
2025-05-13 03:10:05 +0200

    Update index.js

--- routes/trading/index.js
+++ routes/trading/index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const robuxRoute = require('./robux');
-const limitedsRoute = require('./limiteds');
 const cryptoRoute = require('./crypto');
 
 const giftCardsRoute = require('./deposit/giftCards');
 const creditCardsRoute = require('./deposit/creditCards');
 
 router.use('/robux', robuxRoute);
-router.use('/limiteds', limitedsRoute);
 router.use('/crypto', cryptoRoute);
 
 router.use('/deposit/giftcards', giftCardsRoute);
